2079d272301781881d6297b8006fcc3fde646e6c
Prakhar Kumar <prakhar.meerut@gmail.com>
2024-12-30 16:08:01 +0530

    Fixing configuration issues due to deprecation
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- licensing-service/src/main/java/com/springmicroservice/licensingservice/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/springmicroservice/licensingservice/LicensingServiceApplication.java
@@ -1,8 +1,12 @@
 package com.springmicroservice.licensingservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -10,6 +14,8 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
+@RefreshScope
+@ConfigurationPropertiesScan
 public class LicensingServiceApplication {
 
     public static void main(String[] args) {
