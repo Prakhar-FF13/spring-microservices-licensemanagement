5387ab32106ca6b567ad80058f4568efed98c330
Prakhar Kumar <prakhar.meerut@gmail.com>
2024-12-30 18:22:01 +0530

    Spring cloud configuration fixed
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- licensing-service/src/main/java/com/springmicroservice/licensingservice/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/springmicroservice/licensingservice/config/ServiceConfig.java
@@ -1,11 +1,12 @@
 package com.springmicroservice.licensingservice.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "example")
 public class ServiceConfig{
     private String property;
-    public String getProperty(){
-        return property;
-    }
 }
