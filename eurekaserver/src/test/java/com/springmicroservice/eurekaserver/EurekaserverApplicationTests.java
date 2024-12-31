4567fc5bc0d3578c202dbcfabd972825695520af
Prakhar Kumar <prakhar.meerut@gmail.com>
2024-12-31 18:01:26 +0530

    Added eureka service discovery
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

---
+++ eurekaserver/src/test/java/com/springmicroservice/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmicroservice.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
